3484f5190ff4cef567a99f2e20085b5a0dce23a9
ArcaneDev <141244501+Nam4ik@users.noreply.github.com>
2024-10-20 16:20:52 +0300

    Add files via upload

--- TeleBot.py
+++ TeleBot.py
@@ -1,8 +1,11 @@
 import telebot
-import numpy
+import scipy
 import math
 import requests
+from telebot import types
+from typing import Text
 from openai import OpenAI
+import os
 API_TOKEN = '<No_API>'
 bot = TeleBot.(N-API)
 Langs = ['Ru', 'En']
@@ -12,7 +15,8 @@ Output = ['.txt']
 AIs = ['GPT3.5t', 'GPT4m']
 Credits = "Bot created by Arcane Dev team and EvolveAI"
 Set = [Lang,Output,AI]
-
+lightspeed=300000000
+lightspedt=299792458
 client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
@@ -26,20 +30,39 @@ chat_completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
 )
 @bot.message_handler(commands=['help', 'start', 'wave', 'flaser' ,'Cheight', 'lang', 'credits', 'settings', 'AI', 'PreSets'])
-  def welcome(message):
+   def welcome(message):
     bot.reply_to(message, '''
     Я - бот помощник молодого физика cозданный для Sk challenge. 
     Напиши /help для ознакомления со способностями. 
     ''')
-def txt_cnvrt(self):
-    None
-def Wave_script(self):
-    None
+def Wave_script(message):
+    cnvrt = lightspeed / wave
+    m = 1000000
+    mhz = cnvrt / m
+    bot.send_message(chat.id, f'Частота волны равна {round(cnvrt, 1)} Гц или {round(mhz, 1)} Мгц')
+
+
+def txt_cnvrt(self, message):
+    if '/wave' in message:
+      with open('output.txt', 'w') as f:
+       f.write(Wave_script())
+       bot.reply_to(message)
+       bot.send_message(chat.id, f)
+    if '/help' in message:
+        None
+
+
 def Flaser_script(self):
     None
+
+
 def AI():
     None
+
+
 def Settings():
     None
+
+
 def Output():
     None
